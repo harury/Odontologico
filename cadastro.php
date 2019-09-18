9b9fe3aaab674707a2e2ede29354c8ec0d194db4
Matheus Ribeiro <47608636+harury@users.noreply.github.com>
2019-09-18 18:09:33 -0300

    Add files via upload

--- cadastro.php
+++ cadastro.php
@@ -223,17 +223,20 @@
                                     <a class="btn btn-danger btn-sm"  style="color:#fff" href="#" onclick = "excluir(<?php echo $array['id_pessoa']?>)" role="button"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
                                 </td>
                             </tr>
-                            <?php
-                            }
-                        } else {
-                            echo "Nenhum registro encontrado!";
-                        }
-                    }      
-                    ?>
-                    </tbody>
-                </table>
+                       </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+                            <?php
+                }
+                    } else {?>
+                            <div class="alert alert-danger ml-5" role="alert">
+                                  <?php echo "Nenhum registro encontrado!"; ?>
+                            </div>
+                        <?php
+                    }
+                }      
+                ?>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
